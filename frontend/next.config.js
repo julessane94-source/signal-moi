b0f44accf128c8b42165d76fe542c7e176bd22dc
souleymane sane <julessane94@gmail.com>
2026-05-27 13:10:05 +0000

    test: add Cache-Control no-cache for home page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'signal-moi.vercel.app'],
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
