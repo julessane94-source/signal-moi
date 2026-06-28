6c8475b140ae4b5a913199edf500c879d7f2c2f5
julessane94-source <julessane94@gmail.com>
2026-06-28 23:44:31 +0200

    Create sitemap.ts

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,5 @@
+import { MetadataRoute } from 'next'
+export default function sitemap():MetadataRoute.Sitemap { returne [
+{ url:'https://signal-moi.sn',
+ lastModified: new Date(), changeFrequency: 'daily', priority: 1, }, ]
+                                                        }
